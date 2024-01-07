f5e769e576c6fb53631c45819747a9e1b599dba3
Lee <43488305+poiu694@users.noreply.github.com>
2024-01-07 15:54:58 +0900

    feat: setup tailwind enviornment

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">Vite + React</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((prev) => prev + 1)}>
           count is
