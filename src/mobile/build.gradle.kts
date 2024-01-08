02d068182c63dc264631cda67ada44ce16bdf95a
HeewonP825 <7hwon@naver.com>
2024-01-08 20:46:02 +0900

    feat: setting ktlint

--- src/mobile/build.gradle.kts
+++ src/mobile/build.gradle.kts
@@ -9,4 +9,5 @@ plugins {
     id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "7.4.1" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.0.3"
 }
