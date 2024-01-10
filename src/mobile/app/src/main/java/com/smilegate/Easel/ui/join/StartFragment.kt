b9f3ec0d0a5f810aab4502b93733a115e6db8a1f
HeewonP825 <7hwon@naver.com>
2024-01-10 11:24:11 +0900

    chore: edit theme colors

---
+++ src/mobile/app/src/main/java/com/smilegate/Easel/ui/join/StartFragment.kt
@@ -0,0 +1,17 @@
+package com.smilegate.Easel.ui.join
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.smilegate.Easel.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StartFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StartFragment : Fragment() {
+
+}
