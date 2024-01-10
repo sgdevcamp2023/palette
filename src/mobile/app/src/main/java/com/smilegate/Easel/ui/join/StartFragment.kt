2fff62091e98beda929507c920d0c207252120c5
HeewonP825 <7hwon@naver.com>
2024-01-10 18:24:16 +0900

    feat: add startFragment & edit MainActivity, nav_graph

--- src/mobile/app/src/main/java/com/smilegate/Easel/ui/join/StartFragment.kt
+++ src/mobile/app/src/main/java/com/smilegate/Easel/ui/join/StartFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.smilegate.Easel.R
+import com.smilegate.Easel.databinding.FragmentStartBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -13,5 +14,18 @@ import com.smilegate.Easel.R
  * create an instance of this fragment.
  */
 class StartFragment : Fragment() {
+    private lateinit var binding: FragmentStartBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
