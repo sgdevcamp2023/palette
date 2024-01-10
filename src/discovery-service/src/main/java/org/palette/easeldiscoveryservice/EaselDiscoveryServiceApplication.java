b312ea6a769d6e62f178492aa5bda69c1485ad51
210-reverof <sheltonwon@gmail.com>
2024-01-10 13:59:43 +0900

    feat: set discovery

--- src/discovery-service/src/main/java/org/palette/easeldiscoveryservice/EaselDiscoveryServiceApplication.java
+++ src/discovery-service/src/main/java/org/palette/easeldiscoveryservice/EaselDiscoveryServiceApplication.java
@@ -2,8 +2,9 @@ package org.palette.easeldiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class EaselDiscoveryServiceApplication {
 
