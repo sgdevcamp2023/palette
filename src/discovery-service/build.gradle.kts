15557afc57e85738ab2eae555deddf121d037461
210-reverof <sheltonwon@gmail.com>
2024-01-10 13:53:13 +0900

    feat: init project

---
+++ src/discovery-service/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "org.palette"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.0"
+
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/discovery-service/src/main/java/org/palette/easeldiscoveryservice/EaselDiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+package org.palette.easeldiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EaselDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaselDiscoveryServiceApplication.class, args);
+	}
+
+}
