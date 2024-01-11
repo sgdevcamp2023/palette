0b856631be5b448a1b716b623bb20973ba154d86
doxxx <doxxx93@gmail.com>
2024-01-11 19:14:00 +0900

    feat: init auth service

---
+++ src/auth-service/src/main/java/org/palette/easelauthservice/EaselAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.palette.easelauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaselAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaselAuthServiceApplication.class, args);
+	}
+
+}
