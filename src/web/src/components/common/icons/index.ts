41f925c26f8a8a6b874f898a7e52ac8d482fa86e
PoLEE <43488305+poiu694@users.noreply.github.com>
2024-01-11 15:32:33 +0900

    feat: add icon assets and `Icon`, `AccessibleIconButton` components (#21)
    * feat: add icon svg assets
    
    * feat: add vite-svgr-plugin lib
    
    * feat: set svgr option with typescript
    
    * feat: Icon with type, stroke, fill
    
    * fix: add white, black color to ColorType
    
    * feat: AccessibleIconButton for ScreenReader
    
    * chore: add eol to vite-env.dts

---
+++ src/web/src/components/common/icons/index.ts
@@ -0,0 +1,83 @@
+import AddText from './AddText.svg?react';
+import BarChart from './BarChart.svg?react';
+import Bell from './Bell.svg?react';
+import BlingStar from './BlingStar.svg?react';
+import Bookmark from './Bookmark.svg?react';
+import Camera from './Camera.svg?react';
+import Cart from './Cart.svg?react';
+import CirclePerson from './CirclePerson.svg?react';
+import CircleQuestion from './CircleQuestion.svg?react';
+import CircleX from './CircleX.svg?react';
+import Comment from './Comment.svg?react';
+import DownArrow from './DownArrow.svg?react';
+import GIF from './GIF.svg?react';
+import Globe from './Globe.svg?react';
+import Group from './Group.svg?react';
+import Heart from './Heart.svg?react';
+import Hidden from './Hidden.svg?react';
+import Home from './Home.svg?react';
+import Image from './Image.svg?react';
+import LeftStickArrow from './LeftStickArrow.svg?react';
+import LeftUpArrow from './LeftUpArrow.svg?react';
+import List from './List.svg?react';
+import Mail from './Mail.svg?react';
+import Mic from './Mic.svg?react';
+import Minimize from './Minimize.svg?react';
+import Money from './Money.svg?react';
+import More from './More.svg?react';
+import Palette from './Palette.svg?react';
+import Pen from './Pen.svg?react';
+import PurpleStar from './PurpleStar.svg?react';
+import Retweet from './Retweet.svg?react';
+import RightArrow from './RightArrow.svg?react';
+import Search from './Search.svg?react';
+import Setting from './Setting.svg?react';
+import Share from './Share.svg?react';
+import Sun from './Sun.svg?react';
+import ThreeDot from './ThreeDot.svg?react';
+import User from './User.svg?react';
+import Verified from './Verified.svg?react';
+import X from './X.svg?react';
+
+export const ICONS = {
+  addText: AddText,
+  barChart: BarChart,
+  bell: Bell,
+  blingStar: BlingStar,
+  bookmark: Bookmark,
+  camera: Camera,
+  cart: Cart,
+  circlePerson: CirclePerson,
+  circleQuestion: CircleQuestion,
+  circleX: CircleX,
+  comment: Comment,
+  downArrow: DownArrow,
+  gif: GIF,
+  globe: Globe,
+  group: Group,
+  heart: Heart,
+  hidden: Hidden,
+  home: Home,
+  image: Image,
+  leftStickArrow: LeftStickArrow,
+  leftUpArrow: LeftUpArrow,
+  list: List,
+  mail: Mail,
+  mic: Mic,
+  minimize: Minimize,
+  money: Money,
+  more: More,
+  palette: Palette,
+  pen: Pen,
+  purpleStar: PurpleStar,
+  retweet: Retweet,
+  rightArrow: RightArrow,
+  search: Search,
+  setting: Setting,
+  share: Share,
+  sun: Sun,
+  threeDot: ThreeDot,
+  user: User,
+  verified: Verified,
+  x: X,
+} as const;
--- src/web/src/components/common/index.ts
+++ src/web/src/components/common/index.ts
@@ -1,2 +1,4 @@
+export { default as AccessibleIconButton } from './AccessibleIconButton';
 export { default as Button } from './Button';
+export { default as Icon } from './Icon';
 export { default as Typography } from './Typography';
