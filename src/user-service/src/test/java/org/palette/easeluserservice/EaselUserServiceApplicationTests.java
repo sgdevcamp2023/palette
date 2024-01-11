6b4fd8a1519d38336e494d03d66ab6b8014a0451
210-reverof <sheltonwon@gmail.com>
2024-01-11 20:20:59 +0900

    feat: init project

---
+++ src/user-service/src/test/java/org/palette/easeluserservice/EaselUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.palette.easeluserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaselUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
