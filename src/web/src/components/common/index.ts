4669a889a25369dcbb4c8ceac9d59414a8debd8e
Lee <43488305+poiu694@users.noreply.github.com>
2024-01-12 08:52:07 +0900

    feat: Header component

--- src/web/src/components/common/index.ts
+++ src/web/src/components/common/index.ts
@@ -1,4 +1,5 @@
 export { default as AccessibleIconButton } from './AccessibleIconButton';
 export { default as Button } from './Button';
+export { default as Header } from './Header';
 export { default as Icon } from './Icon';
 export { default as Typography } from './Typography';
