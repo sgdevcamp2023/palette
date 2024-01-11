c318d774b7d118fa1456c2b5c697ede509757835
HeewonP825 <7hwon@naver.com>
2024-01-11 11:44:04 +0900

    feat: add nagivation start to login & loginFragment ui xml

--- src/mobile/app/src/main/java/com/smilegate/Easel/ui/join/StartFragment.kt
+++ src/mobile/app/src/main/java/com/smilegate/Easel/ui/join/StartFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.smilegate.Easel.R
 import com.smilegate.Easel.databinding.FragmentStartBinding
 
@@ -16,11 +18,20 @@ import com.smilegate.Easel.databinding.FragmentStartBinding
 class StartFragment : Fragment() {
     private lateinit var binding: FragmentStartBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentStartBinding.inflate(inflater, container, false)
+
+        navController = findNavController()
+
+        binding.startFragmentLoginBtn.setOnClickListener {
+            navController.navigate(R.id.action_startFragment_to_loginFragment)
+        }
+
         return binding.root
     }
 
