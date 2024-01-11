40e9f9c2687763e2f6ff4f51536e1e1ebcf0e7e6
doxxx <doxxx93@gmail.com>
2024-01-11 19:30:57 +0900

    feat: init dm service

---
+++ src/dm-serveice/src/main/java/org/palette/easeldmservice/EaselDmServiceApplication.java
@@ -0,0 +1,13 @@
+package org.palette.easeldmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaselDmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaselDmServiceApplication.class, args);
+	}
+
+}
---
+++ src/dm-serveice/src/test/java/org/palette/easeldmservice/EaselDmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.palette.easeldmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaselDmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
