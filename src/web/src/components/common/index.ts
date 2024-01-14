0529114141a36f99e20686691c85e96441e83241
Lee <43488305+poiu694@users.noreply.github.com>
2024-01-14 19:21:36 +0900

    feat: BottomNavigation

--- src/web/src/components/common/index.ts
+++ src/web/src/components/common/index.ts
@@ -1,4 +1,5 @@
 export { default as AccessibleIconButton } from './AccessibleIconButton';
+export { default as BottomNavigation } from './BottomNavigation';
 export { default as Button } from './Button';
 export { default as ContentLayout } from './ContentLayout';
 export { default as Header } from './Header';
