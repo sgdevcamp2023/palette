20a555ce7e8077aab88196a77dd34c07d82bc5c2
Lee <43488305+poiu694@users.noreply.github.com>
2024-01-16 12:53:06 +0900

    feat: user input password on SignUpPasswordBox

--- src/web/src/components/signup/index.ts
+++ src/web/src/components/signup/index.ts
@@ -1,2 +1,3 @@
 export { default as SignUpEmailBox } from './SignUpEmailBox';
 export { default as SignUpEmailVerifyBox } from './SignUpEmailVerifyBox';
+export { default as SignUpPasswordBox } from './SignUpPasswordBox';
