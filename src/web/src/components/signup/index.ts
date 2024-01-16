45b2ae60f2692865245cd67278c724b59136ad7a
Lee <43488305+poiu694@users.noreply.github.com>
2024-01-16 12:53:51 +0900

    feat: user input profileImage on SignUpProfileImageBox

--- src/web/src/components/signup/index.ts
+++ src/web/src/components/signup/index.ts
@@ -1,3 +1,4 @@
 export { default as SignUpEmailBox } from './SignUpEmailBox';
 export { default as SignUpEmailVerifyBox } from './SignUpEmailVerifyBox';
 export { default as SignUpPasswordBox } from './SignUpPasswordBox';
+export { default as SignUpProfileImageBox } from './SignUpProfileImageBox';
