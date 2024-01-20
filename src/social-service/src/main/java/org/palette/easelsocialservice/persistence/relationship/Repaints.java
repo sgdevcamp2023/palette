a11dafa7b57337cae869c290d6527004f3a1c9b9
210-reverof <sheltonwon@gmail.com>
2024-01-20 10:13:23 +0900

    fix: update id type

--- src/social-service/src/main/java/org/palette/easelsocialservice/persistence/relationship/Repaints.java
+++ src/social-service/src/main/java/org/palette/easelsocialservice/persistence/relationship/Repaints.java
@@ -2,14 +2,14 @@ package org.palette.easelsocialservice.persistence.relationship;
 
 import lombok.Getter;
 import org.palette.easelsocialservice.persistence.domain.Paint;
-import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.RelationshipId;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
 import org.springframework.data.neo4j.core.schema.TargetNode;
 
 @Getter
 @RelationshipProperties
 public class Repaints {
-    @Id
+    @RelationshipId
     private Long id;
 
     @TargetNode
