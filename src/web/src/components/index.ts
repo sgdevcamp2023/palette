e61b6767805c1bbf4481421607e0220012fade72
Lee <43488305+poiu694@users.noreply.github.com>
2024-01-22 16:37:01 +0900

    feat: FloatingButton with useLongPress, animation

--- src/web/src/components/index.ts
+++ src/web/src/components/index.ts
@@ -2,6 +2,7 @@ export * from './common';
 export { default as AccessibleIconButton } from './AccessibleIconButton';
 export { default as AsyncBoundary } from './AsyncBoundary';
 export { default as BottomNavigation } from './BottomNavigation';
+export { default as FloatingButton } from './FloatingButton';
 export { default as Header } from './Header';
 export { default as NotSupportText } from './NotSupportText';
 export { default as StepTitle } from './StepTitle';
