053be0426a07759df98d3834e6afa6c1873361b5
210-reverof <sheltonwon@gmail.com>
2024-01-23 14:02:58 +0900

    feat: add linking link process

--- src/social-service/src/main/java/org/palette/easelsocialservice/persistence/LinkRepository.java
+++ src/social-service/src/main/java/org/palette/easelsocialservice/persistence/LinkRepository.java
@@ -2,6 +2,9 @@ package org.palette.easelsocialservice.persistence;
 
 import org.palette.easelsocialservice.persistence.domain.Link;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 
-public interface LinkRepository extends Neo4jRepository<Link, String> {
+public interface LinkRepository extends Neo4jRepository<Link, Long> {
+    @Query("MATCH (n: Link) RETURN toInteger(n.lid) as lid ORDER BY n.lid DESC LIMIT 1")
+    Long getLastLid();
 }
