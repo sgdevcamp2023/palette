fe086f9f5a023245adf0262819240dc6eabe40ae
Diger <kitten.diger@gmail.com>
2024-01-23 23:53:45 +0900

    feat : GlobalExceptionHandler 추가

---
+++ src/auth-service/src/main/java/org/palette/easelauthservice/exception/ExceptionType.java
@@ -0,0 +1,79 @@
+package org.palette.easelauthservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ExceptionType {
+
+    // 400
+    AUTH_400_000001(
+            "AUTH_400_000001",
+            "INCORRECT_PARAMETER",
+            "요청 파라미터가 올바르지 않습니다.",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    AUTH_400_000002(
+            "AUTH_400_000002",
+            "BROKEN_TOKEN",
+            "요청 토큰이 올바르지 않습니다.",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    // 401
+    AUTH_401_000001(
+            "AUTH_401_000001",
+            "MISSED AUTHENTICATION",
+            "해당 요청은 인증이 필요합니다.",
+            HttpStatus.UNAUTHORIZED
+    ),
+
+    // 403
+    AUTH_403_000001(
+            "AUTH_403_000001",
+            "NOT ALLOWED PERMISSION",
+            "해당 요청에 대한 권한이 없습니다.",
+            HttpStatus.FORBIDDEN
+    ),
+
+    AUTH_403_000002(
+            "AUTH_403_000002",
+            "EXPIRED_TOKEN",
+            "요청 토큰이 만료되었습니다.",
+            HttpStatus.FORBIDDEN
+    ),
+
+    // 404
+    AUTH_404_000001(
+            "AUTH_404_000001",
+            "NOT FOUNDED",
+            "해당 리소스가 존재하지 않습니다.",
+            HttpStatus.NOT_FOUND
+    ),
+
+    // 405
+    AUTH_405_000001(
+            "AUTH_403_000005",
+            "NOT ALLOWED METHOD",
+            "올바르지 않은 요청 메서드입니다.",
+            HttpStatus.METHOD_NOT_ALLOWED
+    ),
+
+    // 409
+    AUTH_409_000001(
+            "AUTH_409_000001",
+            "DUPLICATED",
+            "중복된 리소스가 있습니다.",
+            HttpStatus.CONFLICT
+    ),
+
+    ;
+
+    private final String code;
+    private final String message;
+    private final String description;
+    private final HttpStatus httpStatus;
+}
