8a9c2f28c0685a1b74fab68b6911681f35003005
Diger <kitten.diger@gmail.com>
2024-01-23 16:48:15 +0900

    feat: 인증 번호 생성 기능 추가

---
+++ src/auth-service/src/main/java/org/palette/easelauthservice/config/ApplicationConfig.java
@@ -0,0 +1,15 @@
+package org.palette.easelauthservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public static Random random() {
+        return new Random();
+    }
+}
---
+++ src/auth-service/src/main/java/org/palette/easelauthservice/generator/AuthPayloadGenerator.java
@@ -0,0 +1,20 @@
+package org.palette.easelauthservice.generator;
+
+import lombok.RequiredArgsConstructor;
+import org.palette.easelauthservice.config.ApplicationConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthPayloadGenerator {
+
+    public String execute() {
+        StringBuilder payload = new StringBuilder();
+
+        for (int threshod = 0; threshod < 6; threshod++) {
+            payload.append(ApplicationConfig.random().nextInt(10));
+        }
+
+        return payload.toString();
+    }
+}
