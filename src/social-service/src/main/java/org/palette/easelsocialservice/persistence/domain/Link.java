565d2eca07fdaad587562c9c5cba166c13c63afb
210-reverof <sheltonwon@gmail.com>
2024-01-23 14:02:25 +0900

    feat: add Paint, Link field and

--- src/social-service/src/main/java/org/palette/easelsocialservice/persistence/domain/Link.java
+++ src/social-service/src/main/java/org/palette/easelsocialservice/persistence/domain/Link.java
@@ -6,12 +6,16 @@ import org.springframework.data.neo4j.core.schema.Node;
 @Node
 public class Link {
     @Id
+    private Long lid;
+
     private String shortUrl;
 
     private String originalUrl;
 
-    public Link(String shortUrl, String originalUrl) {
+    public Link(Long lid, String shortUrl, String originalUrl) {
+        this.lid = lid;
         this.shortUrl = shortUrl;
         this.originalUrl = originalUrl;
     }
+
 }
