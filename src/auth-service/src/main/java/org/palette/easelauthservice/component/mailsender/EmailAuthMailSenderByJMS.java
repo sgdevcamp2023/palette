5cce47bbcb81ffa71b25e26ae50058a3b22a5f9d
Diger <kitten.diger@gmail.com>
2024-01-25 18:22:15 +0900

    refactor: 이메일 호스트 주소 환경변수화

--- src/auth-service/src/main/java/org/palette/easelauthservice/component/mailsender/EmailAuthMailSenderByJMS.java
+++ src/auth-service/src/main/java/org/palette/easelauthservice/component/mailsender/EmailAuthMailSenderByJMS.java
@@ -1,7 +1,7 @@
 package org.palette.easelauthservice.component.mailsender;
 
 import lombok.RequiredArgsConstructor;
-import org.palette.easelauthservice.config.MailSenderConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -12,6 +12,9 @@ public class EmailAuthMailSenderByJMS implements EmailAuthMailSender {
     private static final String SUBJECT = "Easel 이메일 인증";
     private static final String AUTH_MAIL_FORM = "이메일 인증번호 : ";
 
+    @Value("${mail-host-address}")
+    private String mailHostAddress;
+
     private final JavaMailSender javaMailSender;
 
     @Override
@@ -20,7 +23,7 @@ public class EmailAuthMailSenderByJMS implements EmailAuthMailSender {
             String authPayload
     ) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("palette1203@naver.com");
+        message.setFrom(mailHostAddress);
         message.setTo(to);
         message.setSubject(SUBJECT);
         message.setText(AUTH_MAIL_FORM + authPayload);
