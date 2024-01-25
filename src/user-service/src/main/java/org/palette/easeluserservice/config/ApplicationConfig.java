b1191488c50ca19a44a98ac602cbc1d17e878778
Diger <kitten.diger@gmail.com>
2024-01-25 19:05:51 +0900

    refactor : 패스워드 유틸라이저

--- src/user-service/src/main/java/org/palette/easeluserservice/config/ApplicationConfig.java
+++ src/user-service/src/main/java/org/palette/easeluserservice/config/ApplicationConfig.java
@@ -1,18 +1,13 @@
 package org.palette.easeluserservice.config;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableDiscoveryClient
 @EnableJpaAuditing
 @EnableAsync
 public class ApplicationConfig {
 
-    @Bean
-    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
