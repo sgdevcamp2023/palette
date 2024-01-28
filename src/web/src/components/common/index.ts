0a1b351e8ce8d653e7341143207c28de903b4e39
Lee <43488305+poiu694@users.noreply.github.com>
2024-01-28 14:54:21 +0900

    feat: CircularProgress

--- src/web/src/components/common/index.ts
+++ src/web/src/components/common/index.ts
@@ -1,6 +1,7 @@
 export * from './Modal';
 export { default as BottomSheet } from './BottomSheetModal';
 export { default as Button } from './Button';
+export { default as CircularProgress } from './CircularProgress';
 export { default as Icon } from './Icon';
 export { default as Input } from './Input';
 export { default as Tabs } from './Tabs';
