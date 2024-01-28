c8c68fb45634e9979d26e75c94235dd27aad573d
Lee <43488305+poiu694@users.noreply.github.com>
2024-01-28 15:21:26 +0900

    feat: converToMedia utility

--- src/web/src/utils/helperPost.ts
+++ src/web/src/utils/helperPost.ts
@@ -3,13 +3,22 @@ import type { EditPaint } from '@/@types';
 /**
  * 기본 값으로 채워주면서 EditPaint 타입으로 만들어줍니다.
  */
-export const forEditPaint = (paint: Partial<EditPaint>): EditPaint => ({
+export const forEditPaint = (
+  paint: Omit<Partial<EditPaint>, 'medias'>,
+): Omit<EditPaint, 'medias'> => ({
   text: paint.text ?? '',
-  medias: paint.medias ?? [],
   taggedUserIds: paint.taggedUserIds ?? [],
   quotePaintId: paint.quotePaintId ?? '',
   inReplyToPaintId: paint.inReplyToPaintId ?? '',
   hashtags: paint.hashtags ?? [],
   mentions: paint.mentions ?? [],
   links: paint.links ?? [],
+});
+
+export const convertToMedia = (
+  url: string,
+  type: EditPaint['medias'][number]['type'],
+): EditPaint['medias'][number] => ({
+  id: url,
+  type,
 });
