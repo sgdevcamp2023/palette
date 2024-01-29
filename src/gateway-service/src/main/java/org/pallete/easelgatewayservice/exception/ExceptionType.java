a29b49232078f2fbcca21d7fb64e36951b685332
Diger <kitten.diger@gmail.com>
2024-01-29 16:40:16 +0900

    refactor : ExceptionType 수정

--- src/gateway-service/src/main/java/org/pallete/easelgatewayservice/exception/ExceptionType.java
+++ src/gateway-service/src/main/java/org/pallete/easelgatewayservice/exception/ExceptionType.java
@@ -9,24 +9,24 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionType {
 
     // 400
-    GATEWAY_000001(
-            "GATEWAY_000001",
+    GATEWAY_400_000001(
+            "GATEWAY_400_000001",
             "INCORRECT_PARAMETER",
             "요청 파라미터가 올바르지 않습니다.",
             HttpStatus.BAD_REQUEST
     ),
 
     // 401
-    GATEWAY_000002(
-            "GATEWAY_000002",
+    GATEWAY_401_000001(
+            "GATEWAY_401_000001",
             "MISSED AUTHENTICATION",
             "해당 요청은 인증이 필요합니다.",
             HttpStatus.UNAUTHORIZED
     ),
 
     // 403
-    GATEWAY_000003(
-            "GATEWAY_000003",
+    GATEWAY_403_000001(
+            "GATEWAY_403_000001",
             "NOT ALLOWED PERMISSION",
             "해당 요청에 대한 권한이 없습니다.",
             HttpStatus.FORBIDDEN
@@ -34,28 +34,36 @@ public enum ExceptionType {
 
 
     // 404
-    GATEWAY_000004(
-            "GATEWAY_000004",
+    GATEWAY_404_000001(
+            "GATEWAY_404_000001",
             "NOT FOUNDED",
             "해당 리소스가 존재하지 않습니다.",
             HttpStatus.NOT_FOUND
     ),
 
     // 405
-    GATEWAY_000005(
-            "GATEWAY_000005",
+    GATEWAY_405_000001(
+            "GATEWAY_405_000001",
             "NOT ALLOWED METHOD",
             "올바르지 않은 요청 메서드입니다.",
             HttpStatus.METHOD_NOT_ALLOWED
     ),
 
     // 409
-    GATEWAY_000006(
-            "GATEWAY_000006",
+    GATEWAY_409_000001(
+            "GATEWAY_409_000001",
             "DUPLICATED",
             "중복된 리소스가 있습니다.",
             HttpStatus.CONFLICT
     ),
+
+    GATEWAY_500_000001(
+            "GATEWAY_500_000001",
+            "INTERNAL_SERVER_ERROR",
+            "서버 내부에 예기치 못한 오류가 발생했습니다.",
+            HttpStatus.INTERNAL_SERVER_ERROR
+    ),
+
 
     ;
 
