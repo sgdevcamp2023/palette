27fdb9efea0464046b6d4205b6bac99fd0cdc9b9
Diger <kitten.diger@gmail.com>
2024-01-30 18:17:17 +0900

    feat : AuthService GrpcServer JwtValidate/Passport발급 기능 추가

--- src/auth-service/src/main/java/org/palette/easelauthservice/external/GrpcServer.java
+++ src/auth-service/src/main/java/org/palette/easelauthservice/external/GrpcServer.java
@@ -4,12 +4,11 @@ import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.palette.easelauthservice.component.generator.emailauth.AuthPayloadGenerator;
-import org.palette.easelauthservice.component.jwt.JwtAgent;
 import org.palette.easelauthservice.component.mailsender.EmailAuthMailSender;
 import org.palette.easelauthservice.redis.RedisEmailAuthService;
-import org.palette.grpc.*;
-import org.palette.passport.PassportGenerator;
-import org.palette.passport.component.UserInfo;
+import org.palette.grpc.GAuthServiceGrpc;
+import org.palette.grpc.GSendEmailAuthRequest;
+import org.palette.grpc.GSendEmailAuthResponse;
 
 @GrpcService
 @RequiredArgsConstructor
@@ -18,9 +17,6 @@ public class GrpcServer extends GAuthServiceGrpc.GAuthServiceImplBase {
     private final RedisEmailAuthService redisEmailAuthService;
     private final EmailAuthMailSender emailAuthMailSender;
     private final AuthPayloadGenerator authPayloadGenerator;
-    private final JwtAgent jwtAgent;
-    private final PassportGenerator passportGenerator;
-    private final GrpcUserClient grpcUserClient;
 
     @Override
     public void sendEmailAuth(
@@ -43,37 +39,6 @@ public class GrpcServer extends GAuthServiceGrpc.GAuthServiceImplBase {
         GSendEmailAuthResponse response = GSendEmailAuthResponse.newBuilder()
                 .setIsSuccess(true)
                 .build();
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-    }
-
-    @Override
-    public void validateJWT(
-            GValidateJWTRequest request,
-            StreamObserver<GValidateJWTResponse> responseObserver
-    ) {
-        Long userId = jwtAgent.parseUserId(request.getJwtPayload());
-
-        GLoadUserInfoFromIdResponse gLoadUserInfoFromIdResponse = grpcUserClient.loadById(userId);
-
-        String passportPayload = passportGenerator.generatePassport(
-                new UserInfo(
-                        userId,
-                        gLoadUserInfoFromIdResponse.getEmail(),
-                        gLoadUserInfoFromIdResponse.getNickname(),
-                        gLoadUserInfoFromIdResponse.getUsername(),
-                        gLoadUserInfoFromIdResponse.getRole(),
-                        gLoadUserInfoFromIdResponse.getIsActivated(),
-                        gLoadUserInfoFromIdResponse.getAccessedAt(),
-                        gLoadUserInfoFromIdResponse.getCreatedAt(),
-                        gLoadUserInfoFromIdResponse.getDeletedAt()
-                )
-        );
-
-        GValidateJWTResponse response = GValidateJWTResponse.newBuilder()
-                .setPassportPayload(passportPayload)
-                .build();
-
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
