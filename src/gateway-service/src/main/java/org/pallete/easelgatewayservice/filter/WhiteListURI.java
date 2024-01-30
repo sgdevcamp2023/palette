6824c033951b9bfd3913561a1326aa7ade302759
Diger <kitten.diger@gmail.com>
2024-01-30 18:40:45 +0900

    refactor : WhiteListURI 수정

--- src/gateway-service/src/main/java/org/pallete/easelgatewayservice/filter/WhiteListURI.java
+++ src/gateway-service/src/main/java/org/pallete/easelgatewayservice/filter/WhiteListURI.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum WhiteListURI {
+    EMAIL_DUPLICATE_VERIFICATION("/users/verify-email"),
+    USERNAME_DUPLICATE_VERIFICATION("/users/verify-username"),
     TEMPORARY_JOIN("/users/temporary-join"),
     EMAIL_AUTH("/auth"),
     JOIN("/users/join"),
