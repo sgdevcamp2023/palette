c49fd90bf88c5599fce6fcc1a8d30cccc1258237
HeewonP825 <7hwon@naver.com>
2024-01-31 15:07:11 +0900

    chore: add eol

--- src/mobile/app/src/main/java/com/smilegate/Easel/domain/model/HighlightItem.kt
+++ src/mobile/app/src/main/java/com/smilegate/Easel/domain/model/HighlightItem.kt
@@ -3,5 +3,5 @@ package com.smilegate.Easel.domain.model
 data class HighlightItem(
     val title: String,
     val description: String,
-    val btnText: String // 예시로 이미지 URL을 사용하였습니다. 실제 프로젝트에 맞게 변경하세요.
+    val btnText: String
 )
