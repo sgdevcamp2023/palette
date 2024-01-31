8e1e9b1d005e3d924d10d579689fd7c528b53995
DoHyeon Kim <kitten.diger@gmail.com>
2024-01-31 16:44:18 +0900

    feat: 웹 클라이언트 로그아웃 기능 추가 (#157)
    * feat : logout 기능 추가
    
    * refactor : dependency typo

--- src/auth-service/src/main/java/org/palette/easelauthservice/controller/AuthController.java
+++ src/auth-service/src/main/java/org/palette/easelauthservice/controller/AuthController.java
@@ -4,13 +4,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.palette.easelauthservice.component.cookie.AuthCookiePair;
 import org.palette.easelauthservice.component.cookie.CookieAgent;
+import org.palette.easelauthservice.component.cookie.LogoutCookiePair;
 import org.palette.easelauthservice.component.jwt.component.JwtPair;
 import org.palette.easelauthservice.dto.request.AuthEmailResendRequest;
 import org.palette.easelauthservice.dto.request.EmailAuthRequest;
 import org.palette.easelauthservice.dto.request.LoginRequest;
 import org.palette.easelauthservice.usecase.AuthUsecase;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -44,6 +48,17 @@ public class AuthController {
         AuthCookiePair authCookie = cookieAgent.createAuthCookie(jwtPair.accessToken(), jwtPair.refreshToken());
         httpServletResponse.addCookie(authCookie.accessTokenCookie());
         httpServletResponse.addCookie(authCookie.refreshTokenCookie());
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(
+            HttpServletResponse httpServletResponse
+    ) {
+        LogoutCookiePair logoutCookie = cookieAgent.createLogoutCookie();
+        httpServletResponse.addCookie(logoutCookie.accessTokenCookie());
+        httpServletResponse.addCookie(logoutCookie.refreshTokenCookie());
 
         return ResponseEntity.ok().build();
     }
