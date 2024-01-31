8d7122948a671112f1b25aeb95ac887e2f90a4bb
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2024-01-31 15:52:38 +0900

    feat: create user like (#160)
    * fix: update build.gradle
    
    * feat : UserService Proto LoadById
    
    * feat: user spring's ObjectMapper
    
    * feat: create user paint like usecase
    
    * feat: create user paint getter from paint service
    
    * fix: fix record mapping exception (TODO: quote relationship)
    
    * feat: create user likes
    
    * test: user paint like test

---
+++ src/social-service/src/main/java/org/palette/easelsocialservice/persistence/relationship/Likes.java
@@ -0,0 +1,29 @@
+package org.palette.easelsocialservice.persistence.relationship;
+
+import lombok.Getter;
+import org.palette.easelsocialservice.persistence.domain.Paint;
+import org.springframework.data.neo4j.core.schema.Relationship;
+import org.springframework.data.neo4j.core.schema.RelationshipId;
+import org.springframework.data.neo4j.core.schema.RelationshipProperties;
+import org.springframework.data.neo4j.core.schema.TargetNode;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+
+@Getter
+@RelationshipProperties
+public class Likes {
+    @RelationshipId
+    private Long id;
+
+    @TargetNode
+    private Paint paint;
+
+    private LocalDateTime createdAt;
+
+    public Likes(final Paint paint) {
+        this.paint = paint;
+        this.createdAt = LocalDateTime.now();
+    }
+}
