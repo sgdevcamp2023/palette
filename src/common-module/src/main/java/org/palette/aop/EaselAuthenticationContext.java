93aa697f3ba1219ff579e3fc325a7a3dc10e331e
Diger <kitten.diger@gmail.com>
2024-01-31 16:04:17 +0900

    refactor : EaselAuthenticationContext 삽입 필드 변경

--- src/common-module/src/main/java/org/palette/aop/EaselAuthenticationContext.java
+++ src/common-module/src/main/java/org/palette/aop/EaselAuthenticationContext.java
@@ -1,11 +1,16 @@
 package org.palette.aop;
 
+import org.palette.passport.component.Passport;
 import org.palette.passport.component.UserInfo;
 
 public class EaselAuthenticationContext {
-    static final ThreadLocal<UserInfo> CONTEXT = new ThreadLocal<>();
+    static final ThreadLocal<Passport> CONTEXT = new ThreadLocal<>();
 
     public static UserInfo getUserInfo() {
-        return CONTEXT.get();
+        return CONTEXT.get().userInfo();
+    }
+
+    public static String getIntegrityKey() {
+        return CONTEXT.get().integrityKey();
     }
 }
