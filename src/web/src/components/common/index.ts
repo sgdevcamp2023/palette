94ccc7cf7a330dd7c72db5ea6c6d7cb6a5b0198f
PoLEE <43488305+poiu694@users.noreply.github.com>
2024-02-02 15:23:20 +0900

    feat: lazy load image when view on screen (#186)
    * feat: useLazyImage hook with intersection observer
    
    * feat: LazyImage component
    
    * feat: apply lazy load image in timeline item box

--- src/web/src/components/common/index.ts
+++ src/web/src/components/common/index.ts
@@ -4,5 +4,6 @@ export { default as Button } from './Button';
 export { default as CircularProgress } from './CircularProgress';
 export { default as Icon } from './Icon';
 export { default as Input } from './Input';
+export { default as LazyImage } from './LazyImage';
 export { default as Tabs } from './Tabs';
 export { default as Typography } from './Typography';
