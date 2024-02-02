7d7120296e05186df69ddf0bb1ab4f55f6bd2b89
Diger <kitten.diger@gmail.com>
2024-02-02 19:59:00 +0900

    refactor : typo

--- src/social-service/src/main/java/org/palette/easelsocialservice/persistence/relationship/Likes.java
+++ src/social-service/src/main/java/org/palette/easelsocialservice/persistence/relationship/Likes.java
@@ -2,14 +2,11 @@ package org.palette.easelsocialservice.persistence.relationship;
 
 import lombok.Getter;
 import org.palette.easelsocialservice.persistence.domain.Paint;
-import org.springframework.data.neo4j.core.schema.Relationship;
 import org.springframework.data.neo4j.core.schema.RelationshipId;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
 import org.springframework.data.neo4j.core.schema.TargetNode;
 
 import java.time.LocalDateTime;
-import java.util.LinkedList;
-import java.util.List;
 
 @Getter
 @RelationshipProperties
