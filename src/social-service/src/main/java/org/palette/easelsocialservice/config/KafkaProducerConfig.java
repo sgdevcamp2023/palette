9fdc0b5c7afcc46b3b34f0258aee14cd744a65ae
Diger <kitten.diger@gmail.com>
2024-02-04 19:39:23 +0900

    refactor : kafka config

--- src/social-service/src/main/java/org/palette/easelsocialservice/config/KafkaProducerConfig.java
+++ src/social-service/src/main/java/org/palette/easelsocialservice/config/KafkaProducerConfig.java
@@ -22,14 +22,11 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Value("${spring.kafka.consumer.group-id}")
-    private String groupId;
-
     @Bean
     public ProducerFactory<String, EaselEvent> factory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(props);
