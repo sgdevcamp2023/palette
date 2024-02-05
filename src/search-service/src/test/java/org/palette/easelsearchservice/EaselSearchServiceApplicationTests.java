48779dc7c225ab1499d4b5dd5e4be7529d0bfb47
210-reverof <sheltonwon@gmail.com>
2024-02-05 19:29:51 +0900

    feat: init project

---
+++ src/search-service/src/test/java/org/palette/easelsearchservice/EaselSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.palette.easelsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaselSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
