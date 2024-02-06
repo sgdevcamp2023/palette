d9f4621913c6d7de0e530cebdcb1fdee5f319ac4
Diger <kitten.diger@gmail.com>
2024-02-06 16:02:07 +0900

    feat : Detail Event 추가

--- src/common-module/src/main/java/org/palette/dto/event/PaintCreatedEvent.java
+++ src/common-module/src/main/java/org/palette/dto/event/PaintCreatedEvent.java
@@ -22,26 +22,6 @@ public record PaintCreatedEvent(
 
 ) implements EaselEvent {
 
-    public static PaintCreatedEvent build(
-            Long userId,
-            Long paintId,
-            String content,
-            List<HashtagRecord> hashtagRecords,
-            List<MediaRecord> mediaRecords,
-            List<LinkRecord> linkRecords,
-            List<UserRecord> userRecords
-    ) {
-        return PaintCreatedEvent.builder()
-                .writerId(userId)
-                .paintId(paintId)
-                .paintContent(content)
-                .hashtagRecords(hashtagRecords)
-                .mediaRecords(mediaRecords)
-                .linkRecords(linkRecords)
-                .userRecords(userRecords)
-                .build();
-    }
-
     @Override
     public String getTopic() {
         return TopicConstant.PAINT_CREATED.value;
