be75577652a7bd1b0db698582b45ee7b110e35d5
210-reverof <sheltonwon@gmail.com>
2024-02-06 14:29:25 +0900

    feat: add isFollowing field

--- src/social-service/src/main/java/org/palette/easelsocialservice/dto/response/UserResponse.java
+++ src/social-service/src/main/java/org/palette/easelsocialservice/dto/response/UserResponse.java
@@ -4,6 +4,8 @@ import org.palette.easelsocialservice.persistence.domain.User;
 import org.palette.easelsocialservice.persistence.relationship.TagsUser;
 
 public record UserResponse(
+
+        Boolean isFollowing,
         Long id,
         String username,
         String nickname,
@@ -13,6 +15,7 @@ public record UserResponse(
     public static UserResponse from(TagsUser taggedUser) {
         final User user = taggedUser.getUser();
         return new UserResponse(
+                false,
                 user.getUid(),
                 user.getUsername(),
                 user.getNickname(),
@@ -23,6 +26,7 @@ public record UserResponse(
 
     public static UserResponse from(User user) {
         return new UserResponse(
+                false,
                 user.getUid(),
                 user.getUsername(),
                 user.getNickname(),
