5c0b180b7dab58aa4603de7dac43637da853742c
210-reverof <sheltonwon@gmail.com>
2024-02-08 15:47:25 +0900

    feat: update PaintCreatedEvent by adding missing fields

--- src/common-module/src/main/java/org/palette/dto/event/PaintCreatedEvent.java
+++ src/common-module/src/main/java/org/palette/dto/event/PaintCreatedEvent.java
@@ -12,13 +12,21 @@ import java.util.List;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record PaintCreatedEvent(
-        Long writerId,
-        Long paintId,
-        String paintContent,
+        Long id,
+        Boolean isReply,
+        Long authorId,
+        String authorUsername,
+        String authorNickname,
+        String authorImagePath,
+        String authorStatus,
+        PaintCreatedEvent quotePaint,
+        LocalDateTime createdAt,
+        String text,
         List<HashtagRecord> hashtagRecords,
+        List<MentionRecord> mentionRecords
+        List<UserRecord> taggedUserRecords
         List<MediaRecord> mediaRecords,
-        List<LinkRecord> linkRecords,
-        List<UserRecord> userRecords
+        List<LinkRecord> linkRecords
 
 ) implements EaselEvent {
 
