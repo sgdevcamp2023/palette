5d197528d0becd7d1dd2db73398575e9ae3e8f20
210-reverof <sheltonwon@gmail.com>
2024-02-14 00:29:25 +0900

    chore: remove blank

--- src/search-service/src/main/java/org/palette/easelsearchservice/dto/response/UserResponse.java
+++ src/search-service/src/main/java/org/palette/easelsearchservice/dto/response/UserResponse.java
@@ -1,12 +1,11 @@
 package org.palette.easelsearchservice.dto.response;
 
 public record UserResponse(
-
-        Boolean isFollowing,
         Long id,
         String username,
         String nickname,
         String imagePath,
-        String status
+        String status,
+        Boolean isFollowing
 ) {
 }
