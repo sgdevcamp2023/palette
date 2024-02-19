0083e2eb02d4b282b445025f0d94d87d6c9f1e38
Diger <kitten.diger@gmail.com>
2024-02-19 15:33:39 +0900

    refactor : deprecate tomcat

--- src/gateway-service/build.gradle.kts
+++ src/gateway-service/build.gradle.kts
@@ -27,7 +27,11 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.springframework.boot:spring-boot-starter-web"){
+        exclude("org.springframework.boot", "spring-boot-starter-tomcat")
+    }
+
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 
     compileOnly("org.projectlombok:lombok")
