5f3a7fb15bc01e6e3cdf108c801a8a406d8a9a58
doxxx93 <doxxx93@gmail.com>
2024-02-20 00:08:30 +0900

    feat: Email Auth Expiration event

--- src/auth-service/src/main/java/org/palette/easelauthservice/config/RedisConfig.java
+++ src/auth-service/src/main/java/org/palette/easelauthservice/config/RedisConfig.java
@@ -1,16 +1,25 @@
 package org.palette.easelauthservice.config;
 
+import lombok.RequiredArgsConstructor;
+import org.palette.easelauthservice.redis.event.EmailAuthExpirationListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyExpiredEvent;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 @EnableRedisRepositories
+@RequiredArgsConstructor
 public class RedisConfig {
+
+    private final EmailAuthExpirationListener listener;
 
     @Value("${spring.data.redis.host}")
     private String redisHost;
@@ -29,4 +38,20 @@ public class RedisConfig {
         return lettuceConnectionFactory;
     }
 
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(
+                (message, pattern) -> listener.onApplicationEvent(
+                        new RedisKeyExpiredEvent<>(message.getBody())),
+                new PatternTopic("__keyevent@*__:expired")
+        );
+        return container;
+    }
+
+    @Bean
+    public KeyExpirationEventMessageListener keyExpirationEventMessageListener(RedisMessageListenerContainer listenerContainer) {
+        return new KeyExpirationEventMessageListener(listenerContainer);
+    }
 }
