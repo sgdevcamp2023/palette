a75433420eb69dc74754a5fa623fd0d5478ef070
Diger <kitten.diger@gmail.com>
2024-02-19 23:24:16 +0900

    refactor : typo

--- src/gateway-service/src/main/java/org/pallete/easelgatewayservice/config/FeignResponseDecoderConfig.java
+++ src/gateway-service/src/main/java/org/pallete/easelgatewayservice/config/FeignResponseDecoderConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignResponseDecoderConfig {
-    private ObjectFactory<HttpMessageConverters> messageConverters = HttpMessageConverters::new;
+    private final ObjectFactory<HttpMessageConverters> messageConverters = HttpMessageConverters::new;
 
     @Bean
     SpringFormEncoder feignFormEncoder() {
