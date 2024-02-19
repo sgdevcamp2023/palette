58dad3e795da28f68dc8eadf3f73f82c2ee1efc5
Lee <43488305+poiu694@users.noreply.github.com>
2024-02-19 15:59:33 +0900

    feat: profile highlight component

--- src/web/src/components/ProfileTabs.tsx
+++ src/web/src/components/ProfileTabs.tsx
@@ -1,8 +1,10 @@
-import { Tabs } from './common';
+import { toast } from 'react-toastify';
+
 import AsyncBoundary from './AsyncBoundary';
 import ContentLayout from './ContentLayout';
-import ErrorWithResetBox from './ErrorWithResetBox';
 import TimelineItemList from './TimelineItemList';
+import ErrorWithResetBox from './ErrorWithResetBox';
+import { Button, Tabs, Typography } from './common';
 import { TimelineItemListSkeleton } from './skeleton';
 
 interface ProfileTabsProps {
@@ -62,16 +64,25 @@ function ProfileTabs({ className }: ProfileTabsProps) {
               as="section"
               className="mt-0 pl-[12px] pr-[4px] max-h-none"
             >
-              <AsyncBoundary
-                pendingFallback={<TimelineItemListSkeleton />}
-                rejectedFallback={(props) => (
-                  <div className="py-10 pb-32">
-                    <ErrorWithResetBox {...props} />
-                  </div>
-                )}
-              >
-                <TimelineItemList type="post" />
-              </AsyncBoundary>
+              <div className="pt-10 px-6">
+                <Typography size="headline-2" color="grey-600">
+                  프로필에 하이라이트 추가
+                </Typography>
+                <Typography
+                  size="body-2"
+                  color="blueGrey-800"
+                  className="mt-[14px]"
+                >
+                  프로필에 게시물을 하이라이트 하려면 Premium을 구독해야 합니다.
+                </Typography>
+
+                <Button
+                  className="w-[190px] mt-[28px] text-[16px]"
+                  onClick={() => toast('아직 지원되지 않는 기능입니다.')}
+                >
+                  Premium 구독하기
+                </Button>
+              </div>
             </ContentLayout>
           ),
         },
