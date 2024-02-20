f426ace84908ca303ab4c3ee5934978ca4e6dc5c
HeewonP825 <7hwon@naver.com>
2024-02-20 14:34:23 +0900

    feat: add MultiView(quote text & image) in RV

--- src/mobile/app/src/main/java/com/smilegate/Easel/presentation/adapter/TimelineRecyclerViewAdapter.kt
+++ src/mobile/app/src/main/java/com/smilegate/Easel/presentation/adapter/TimelineRecyclerViewAdapter.kt
@@ -13,6 +13,8 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.smilegate.Easel.MainActivity
 import com.smilegate.Easel.R
+import com.smilegate.Easel.databinding.ItemQuoteImageBinding
+import com.smilegate.Easel.databinding.ItemQuoteTextBinding
 import com.smilegate.Easel.databinding.ItemTimelineBinding
 import com.smilegate.Easel.domain.model.TimelineItem
 import com.smilegate.Easel.presentation.TimelinePopupManager
@@ -21,16 +23,33 @@ import com.smilegate.Easel.presentation.view.timeline.ShareBottomSheetDialog
 import com.smilegate.Easel.presentation.view.timeline.ViewBottomSheetDialog
 
 class TimelineRecyclerViewAdapter(private val context: Context, private val timelineList: List<TimelineItem>) :
-    RecyclerView.Adapter<TimelineRecyclerViewAdapter.TimelineViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val popupManager = TimelinePopupManager(context)
     private var timelineListItem: MutableList<TimelineItem> = mutableListOf()
+
+    // 뷰 유형 상수 정의
+    companion object {
+        const val VIEW_TYPE_ORIGINAL = 0
+        const val VIEW_TYPE_TEXT = 1
+        const val VIEW_TYPE_IMAGE = 2
+    }
 
     fun updateData(newList: List<TimelineItem>) {
         timelineListItem = newList.toMutableList()
         notifyDataSetChanged()
     }
-    @RequiresApi(Build.VERSION_CODES.S)
+
+    override fun getItemViewType(position: Int): Int {
+        // 각 아이템의 뷰 타입 반환
+        return when (position % 3) {
+            0 -> VIEW_TYPE_ORIGINAL
+            1 -> VIEW_TYPE_TEXT
+            2 -> VIEW_TYPE_IMAGE
+            else -> throw IllegalArgumentException("Invalid position")
+        }
+    }
+
     inner class TimelineViewHolder(private val binding: ItemTimelineBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
@@ -97,18 +116,191 @@ class TimelineRecyclerViewAdapter(private val context: Context, private val time
             }
         }
     }
-
     @RequiresApi(Build.VERSION_CODES.S)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimelineViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemTimelineBinding.inflate(inflater, parent, false)
-        return TimelineViewHolder(binding)
+    inner class QuoteTextViewHolder(private val binding: ItemQuoteTextBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+            private val icElseActions: ImageView = binding.root.findViewById(R.id.ic_else_actions)
+            private val icViews: ImageView = binding.root.findViewById(R.id.ic_timeline_views)
+            private val icRetweet: ImageView = binding.root.findViewById(R.id.ic_timeline_retweet)
+            private val icShare: ImageView = binding.root.findViewById(R.id.ic_timeline_share)
+
+            init {
+                icElseActions.setOnClickListener {
+                    popupManager.showPopupMenu(icElseActions)
+                }
+
+                icRetweet.setOnClickListener {
+                    retweetBottomSheet()
+                }
+
+                icShare.setOnClickListener {
+                    shareBottomSheet()
+                }
+
+                icViews.setOnClickListener {
+                    viewBottomSheet()
+                }
+            }
+
+            fun bind(item: TimelineItem) {
+                item.profileImg?.let { binding.ivTimelineProfileImg.setImageResource(it) }
+                binding.tvTimelineNickname.text = item.nickName
+                binding.tvTimelineUsername.text = item.userName
+                binding.tvTimelineTime.text = item.timeAgo
+                binding.tvTimelineContent.text = item.content
+                item.contentImg?.let { binding.ivTimelineContentImg.setImageResource(it) }
+                binding.tvTimelineHashtag.text = item.hashtag
+                binding.tvTimelineMention.text = item.replys?.toString() ?: "0"
+                binding.tvTimelineRetweet.text = item.reposts?.toString() ?: "0"
+                binding.tvTimelineLike.text = item.like?.toString() ?: "0"
+                binding.tvTimelineViews.text = item.views?.toString() ?: "0"
+
+                item.quoteProfileImg?.let { binding.ivQuoteProfileImg.setImageResource(it)}
+                binding.tvQuoteNickname.text = item.quoteNickName
+                binding.tvQuoteUsername.text = item.quoteUserName
+                binding.tvQuoteTime.text = item.quoteTimeAgo
+                binding.tvQuoteText.text = item.quoteContent
+                item.quoteContentImg?.let { binding.ivQuoteContentImg.setImageResource(it) }
+
+                binding.tvTimelineContent.setVisibleOrGone(!item.content.isNullOrEmpty())
+                binding.ivTimelineContentImg.setVisibleOrGone(item.contentImg != null)
+                binding.ivTimelineContentCard.setVisibleOrGone(item.contentImg != null)
+
+                binding.tvTimelineHashtag.setVisibleOrGone(!item.hashtag.isNullOrEmpty())
+                binding.tvTimelineMention.setVisibleOrInvisible(item.replys != null)
+                binding.tvTimelineRetweet.setVisibleOrInvisible(item.reposts != null)
+                binding.tvTimelineLike.setVisibleOrInvisible(item.like != null)
+                binding.tvTimelineViews.setVisibleOrInvisible(item.views != null)
+
+                // Glide를 사용하여 프로필 이미지 로드
+                item.profileImg?.let {
+                    Glide.with(binding.root.context)
+                        .load(it)
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(binding.ivTimelineProfileImg)
+                }
+
+                // contentImgUrl이 null이 아닌 경우에만 Glide를 사용하여 이미지 로드
+                item.contentImg?.let {
+                    Glide.with(binding.root.context)
+                        .load(it)
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                        .into(binding.ivTimelineContentImg)
+                }
+            }
+    }
+
+    inner class QuoteImageViewHolder(private val binding: ItemQuoteImageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        private val icElseActions: ImageView = binding.root.findViewById(R.id.ic_else_actions)
+        private val icViews: ImageView = binding.root.findViewById(R.id.ic_timeline_views)
+        private val icRetweet: ImageView = binding.root.findViewById(R.id.ic_timeline_retweet)
+        private val icShare: ImageView = binding.root.findViewById(R.id.ic_timeline_share)
+
+        init {
+            icElseActions.setOnClickListener {
+                popupManager.showPopupMenu(icElseActions)
+            }
+
+            icRetweet.setOnClickListener {
+                retweetBottomSheet()
+            }
+
+            icShare.setOnClickListener {
+                shareBottomSheet()
+            }
+
+            icViews.setOnClickListener {
+                viewBottomSheet()
+            }
+        }
+
+        fun bind(item: TimelineItem) {
+            item.profileImg?.let { binding.ivTimelineProfileImg.setImageResource(it) }
+            binding.tvTimelineNickname.text = item.nickName
+            binding.tvTimelineUsername.text = item.userName
+            binding.tvTimelineTime.text = item.timeAgo
+            binding.tvTimelineContent.text = item.content
+            item.contentImg?.let { binding.ivTimelineContentImg.setImageResource(it) }
+            binding.tvTimelineHashtag.text = item.hashtag
+            binding.tvTimelineMention.text = item.replys?.toString() ?: "0"
+            binding.tvTimelineRetweet.text = item.reposts?.toString() ?: "0"
+            binding.tvTimelineLike.text = item.like?.toString() ?: "0"
+            binding.tvTimelineViews.text = item.views?.toString() ?: "0"
+
+            item.quoteProfileImg?.let { binding.ivQuoteProfileImg.setImageResource(it)}
+            binding.tvQuoteNickname.text = item.quoteNickName
+            binding.tvQuoteUsername.text = item.quoteUserName
+            binding.tvQuoteTime.text = item.quoteTimeAgo
+            binding.tvQuoteText.text = item.quoteContent
+            item.quoteContentImg?.let { binding.ivQuoteProfileImg.setImageResource(it) }
+
+            binding.tvTimelineContent.setVisibleOrGone(!item.content.isNullOrEmpty())
+            binding.ivTimelineContentImg.setVisibleOrGone(item.contentImg != null)
+            binding.ivTimelineContentCard.setVisibleOrGone(item.contentImg != null)
+
+            binding.tvTimelineHashtag.setVisibleOrGone(!item.hashtag.isNullOrEmpty())
+            binding.tvTimelineMention.setVisibleOrInvisible(item.replys != null)
+            binding.tvTimelineRetweet.setVisibleOrInvisible(item.reposts != null)
+            binding.tvTimelineLike.setVisibleOrInvisible(item.like != null)
+            binding.tvTimelineViews.setVisibleOrInvisible(item.views != null)
+
+            // Glide를 사용하여 프로필 이미지 로드
+            item.profileImg?.let {
+                Glide.with(binding.root.context)
+                    .load(it)
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(binding.ivTimelineProfileImg)
+            }
+
+            // contentImgUrl이 null이 아닌 경우에만 Glide를 사용하여 이미지 로드
+            item.contentImg?.let {
+                Glide.with(binding.root.context)
+                    .load(it)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(binding.ivTimelineContentImg)
+            }
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.S)
-    override fun onBindViewHolder(holder: TimelineViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_ORIGINAL -> {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ItemTimelineBinding.inflate(inflater, parent, false)
+                TimelineViewHolder(binding)
+            }
+            VIEW_TYPE_TEXT -> {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ItemQuoteTextBinding.inflate(inflater, parent, false)
+                QuoteTextViewHolder(binding)
+            }
+            VIEW_TYPE_IMAGE -> {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ItemQuoteImageBinding.inflate(inflater, parent, false)
+                QuoteImageViewHolder(binding)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val timelineItem = timelineList[position]
-        holder.bind(timelineItem)
+        when (holder) {
+            is TimelineViewHolder -> {
+                holder.bind(timelineItem)
+            }
+            is QuoteTextViewHolder -> {
+                holder.bind(timelineItem)
+            }
+            is QuoteImageViewHolder -> {
+                holder.bind(timelineItem)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
--- src/mobile/app/src/main/java/com/smilegate/Easel/presentation/view/timeline/FollowingFragment.kt
+++ src/mobile/app/src/main/java/com/smilegate/Easel/presentation/view/timeline/FollowingFragment.kt
@@ -15,6 +15,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.smilegate.Easel.MainActivity
+import com.smilegate.Easel.R
 import com.smilegate.Easel.databinding.FragmentFollowingBinding
 import com.smilegate.Easel.domain.model.TimelineItem
 import com.smilegate.Easel.presentation.adapter.TimelineRecyclerViewAdapter
@@ -109,51 +110,83 @@ class FollowingFragment : Fragment() {
     }
 
     private fun generateDummyTimelineData(): List<TimelineItem> {
-        val profileImgId = com.smilegate.Easel.R.drawable.sample_profile_img5
-        val profileImgId1 = com.smilegate.Easel.R.drawable.sample_profile_img1
-        val profileImgId2 = com.smilegate.Easel.R.drawable.sample_profile_img2
-        val profileImgId3 = com.smilegate.Easel.R.drawable.sample_profile_img3
-        val profileImgId4 = com.smilegate.Easel.R.drawable.sample_profile_img4
+        val profileImgId = R.drawable.sample_profile_img5
+        val profileImgId1 = R.drawable.sample_profile_img1
+        val profileImgId2 = R.drawable.sample_profile_img2
+        val contentImgId = R.drawable.sample_content_img1
+        val contentImgId1 = R.drawable.sample_content_img2
+        val contentImgId2 = R.drawable.sample_content_img3
 
-        val contentImgId = com.smilegate.Easel.R.drawable.sample_content_img1
-        val contentImgId1 = com.smilegate.Easel.R.drawable.sample_content_img2
-        val contentImgId2 = com.smilegate.Easel.R.drawable.sample_content_img3
-        val contentImgId3 = com.smilegate.Easel.R.drawable.sample_content_img4
+        val timelineList = mutableListOf<TimelineItem>()
 
-        val timelineList = listOf(
-            TimelineItem(profileImgId, "이원영", "@courtney81819", "1시간",
-                "아 슈뢰딩거가 아닌가?ㅋ", null, null,
-                2, 1, null, 24),
+        for (i in 0 until 8) {
+            val viewType = i % 3 // 랜덤한 뷰 타입 결정
 
+            val timelineItem = when (viewType) {
+                0 -> TimelineItem(
+                    viewType,
+                    profileImgId1,
+                    "박희원",
+                    "@_Parking1_",
+                    "18분",
+                    "타래 스타트",
+                    contentImgId1,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    1,
+                    1,
+                    null,
+                    114,
+                )
+                1 -> TimelineItem(
+                    viewType,
+                    profileImgId2,
+                    "이상민",
+                    "@isangmi92157279",
+                    "32분",
+                    "비가 내리는 날이에요.\n추적이는 바닥을 보며 걷다보니 카페가 나와 커피를 사 봤어요.",
+                    contentImgId,
+                    null,
+                    profileImgId,
+                    "이원영",
+                    "@courtney81819",
+                    "21시간 전",
+                    "엥 실화?",
+                    contentImgId1,
+                    null,
+                    4,
+                    2,
+                    5,
+                )
+                else -> TimelineItem(
+                    viewType,
+                    profileImgId,
+                    "이원영",
+                    "@courtney81819",
+                    "1시간",
+                    "아 슈뢰딩거가 아닌가?ㅋ",
+                    null,
+                    null,
+                    profileImgId,
+                    "이원영",
+                    "@courtney81819",
+                    "21시간 전",
+                    "엥 실화?",
+                    contentImgId1,
+                    2,
+                    1,
+                    null,
+                    24,
+                )
+            }
 
-            TimelineItem(profileImgId2, "이상민", "@isangmi92157279", "32분",
-                "비가 내리는 날이에요.\n추적이는 바닥을 보며 걷다보니 카페가 나와 커피를 사 봤어요.", contentImgId, null,
-                4, 2, 5, 121),
-
-            TimelineItem(profileImgId1, "박희원", "@_Parking1_", "18분",
-                "타래 스타트", contentImgId1, null,
-                1, 1, null, 114),
-
-            TimelineItem(profileImgId4, "김도율", "@doxxx93", "8분",
-                "테스트", null, null,
-                1, null, 2, 32),
-
-            TimelineItem(profileImgId2, "이상민", "@isangmi92157279", "1주",
-                "커피는 아이스 아메리카노에요.\n컵에 스며든 물방울처럼, 제 마음을 촉촉하게 만들어 주네요..^^", contentImgId2, null,
-                3, 4, 2, 89),
-
-            TimelineItem(profileImgId3, "김도현", "@KittenDiger", "4일",
-                "테스트 인용의 인용", null, null,
-                1, null, null, 30),
-
-            TimelineItem(profileImgId, "이원영", "@courtney81819", "3시간",
-                "청하 로제", contentImgId3, null,
-                3, null, 2, 334),
-
-            TimelineItem(profileImgId3, "김도현", "@KittenDiger", "4일",
-                "구글인ㅇㅎㅇ의 인용의 인용", null, null,
-                1, 2, null, 50),
-        )
+            timelineList.add(timelineItem)
+        }
 
         return timelineList.shuffled()
     }
