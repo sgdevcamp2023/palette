6b2ff18bf2cf274a29bca059f6a4d41aef0030a2
Diger <kitten.diger@gmail.com>
2024-02-20 16:10:10 +0900

    feat : User Created Event Pub In join

--- src/user-service/src/main/java/org/palette/easeluserservice/usecase/UserUsecase.java
+++ src/user-service/src/main/java/org/palette/easeluserservice/usecase/UserUsecase.java
@@ -3,6 +3,7 @@ package org.palette.easeluserservice.usecase;
 import lombok.RequiredArgsConstructor;
 import org.palette.dto.event.TemporaryUserDeletionEvent;
 import org.palette.dto.event.UpdateUserEvent;
+import org.palette.dto.event.UserCreatedEvent;
 import org.palette.easeluserservice.dto.request.EditProfileRequest;
 import org.palette.easeluserservice.dto.request.JoinRequest;
 import org.palette.easeluserservice.dto.request.TemporaryJoinRequest;
@@ -79,6 +80,16 @@ public class UserUsecase {
 
         gRPCSocialClient.createSocialUser(user);
         grpcNotificationClient.createNotificationUser(user);
+
+        kafkaProducer.execute(
+                new UserCreatedEvent(
+                        user.getId(),
+                        user.getProfile().nickname(),
+                        user.getUsername(),
+                        user.getProfile().introduce(),
+                        user.getProfile().staticContentPath().profileImagePath()
+                )
+        );
     }
 
     public RetrieveUserResponse retrieveOther(
