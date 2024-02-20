2a3287f6d9ed490923b742a3b7ed0b87b9bf3c57
Diger <kitten.diger@gmail.com>
2024-02-20 15:16:03 +0900

    feat : ES 유저 저장 기능 추가

--- src/search-service/src/main/java/org/palette/easelsearchservice/external/EventConsumer.java
+++ src/search-service/src/main/java/org/palette/easelsearchservice/external/EventConsumer.java
@@ -1,7 +1,10 @@
 package org.palette.easelsearchservice.external;
 
 import lombok.RequiredArgsConstructor;
+import org.palette.aop.InternalErrorLogging;
 import org.palette.dto.event.PaintCreatedEvent;
+import org.palette.dto.event.UpdateUserEvent;
+import org.palette.dto.event.UserCreatedEvent;
 import org.palette.easelsearchservice.usecase.SearchUsecase;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -11,9 +14,24 @@ import org.springframework.stereotype.Component;
 public class EventConsumer {
     private final SearchUsecase searchUsecase;
 
+    @InternalErrorLogging
     @KafkaListener(topics = "paint_created", groupId = "${spring.kafka.consumer.group-id}",
             containerFactory = "kafkaListenerContainerFactory")
     public void consumePaintCreatedEvent(PaintCreatedEvent paintCreatedEvent) {
         searchUsecase.paintCreated(paintCreatedEvent);
+    }
+
+    @InternalErrorLogging
+    @KafkaListener(topics = "user_created", groupId = "${spring.kafka.consumer.group-id}",
+            containerFactory = "kafkaListenerContainerFactory")
+    public void consumeUserCreatedEvent(UserCreatedEvent userCreatedEvent) {
+        searchUsecase.createUser(userCreatedEvent);
+    }
+
+    @InternalErrorLogging
+    @KafkaListener(topics = "user_created", groupId = "${spring.kafka.consumer.group-id}",
+            containerFactory = "kafkaListenerContainerFactory")
+    public void consumeUserUpdatedEvent(UpdateUserEvent updateUserEvent) {
+        searchUsecase.updateUser(updateUserEvent);
     }
 }
