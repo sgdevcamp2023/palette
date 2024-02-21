f071c65fc30e0188ee8ca813fdbdf643a8fe4ada
Heewon Park <80496838+HeewonP825@users.noreply.github.com>
2024-02-21 15:52:09 +0900

    feat: add quote & detail fragment (#279)
    * feat: add quote text & image ver
    
    * feat: add MultiView(quote text & image) in RV
    
    * feat: edit MultiView(quote text & image) margin & border in RV
    
    * feat: set Visibility for MultiView(quote text & image) in RV
    
    * feat: add margin for MultiView(quote text & image) in RV
    
    * feat: add postDetailFragment
    
    * feat: add item_post_detail.xml
    
    * feat: edit item_post_detail.xml

--- src/mobile/app/build.gradle.kts
+++ src/mobile/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 
     defaultConfig {
--- src/mobile/app/src/main/java/com/smilegate/Easel/presentation/view/profile/MediaFragment.kt
+++ src/mobile/app/src/main/java/com/smilegate/Easel/presentation/view/profile/MediaFragment.kt
@@ -73,49 +73,82 @@ class MediaFragment : Fragment() {
         val profileImgId = R.drawable.sample_profile_img5
         val profileImgId1 = R.drawable.sample_profile_img1
         val profileImgId2 = R.drawable.sample_profile_img2
-        val profileImgId3 = R.drawable.sample_profile_img3
-        val profileImgId4 = R.drawable.sample_profile_img4
-
         val contentImgId = R.drawable.sample_content_img1
         val contentImgId1 = R.drawable.sample_content_img2
         val contentImgId2 = R.drawable.sample_content_img3
-        val contentImgId3 = R.drawable.sample_content_img4
 
-        return listOf(
-            TimelineItem(profileImgId, "이원영", "@courtney81819", "1시간",
-                "아 슈뢰딩거가 아닌가?ㅋ", null, null,
-                2, 1, null, 24),
+        val timelineList = mutableListOf<TimelineItem>()
 
+        for (i in 0 until 8) {
+            val viewType = i % 3 // 랜덤한 뷰 타입 결정
 
-            TimelineItem(profileImgId2, "이상민", "@isangmi92157279", "32분",
-                "비가 내리는 날이에요.\n추적이는 바닥을 보며 걷다보니 카페가 나와 커피를 사 봤어요.", contentImgId, null,
-                4, 2, 5, 121),
+            val timelineItem = when (viewType) {
+                0 -> TimelineItem(
+                    viewType,
+                    profileImgId,
+                    "이원영",
+                    "@courtney81819",
+                    "1시간",
+                    "아 슈뢰딩거가 아닌가?ㅋ",
+                    null,
+                    null,
+                    profileImgId,
+                    "이원영",
+                    "@courtney81819",
+                    "21시간 전",
+                    "엥 실화?",
+                    contentImgId1,
+                    2,
+                    1,
+                    null,
+                    24,
+                )
+                1 -> TimelineItem(
+                    viewType,
+                    profileImgId2,
+                    "이상민",
+                    "@isangmi92157279",
+                    "32분",
+                    "비가 내리는 날이에요.\n추적이는 바닥을 보며 걷다보니 카페가 나와 커피를 사 봤어요.",
+                    contentImgId,
+                    null,
+                    profileImgId,
+                    "이원영",
+                    "@courtney81819",
+                    "21시간 전",
+                    "엥 실화?",
+                    contentImgId1,
+                    null,
+                    4,
+                    2,
+                    5,
+                )
+                else -> TimelineItem(
+                    viewType,
+                    profileImgId1,
+                    "박희원",
+                    "@_Parking1_",
+                    "18분",
+                    "타래 스타트",
+                    contentImgId1,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    1,
+                    1,
+                    null,
+                    114,
+                )
+            }
 
-            TimelineItem(profileImgId1, "박희원", "@_Parking1_", "18분",
-                "타래 스타트", contentImgId1, null,
-                1, 1, null, 114),
+            timelineList.add(timelineItem)
+        }
 
-            TimelineItem(profileImgId4, "김도율", "@doxxx93", "8분",
-                "테스트", null, null,
-                1, null, 2, 32),
-
-            TimelineItem(profileImgId2, "이상민", "@isangmi92157279", "1주",
-                "커피는 아이스 아메리카노에요.\n컵에 스며든 물방울처럼, 제 마음을 촉촉하게 만들어 주네요..^^", contentImgId2, null,
-                3, 4, 2, 89),
-
-            TimelineItem(profileImgId3, "김도현", "@KittenDiger", "4일",
-                "테스트 인용의 인용", null, null,
-                1, null, null, 30),
-
-            TimelineItem(profileImgId, "이원영", "@courtney81819", "3시간",
-                "청하 로제", contentImgId3, null,
-                3, null, 2, 334),
-
-            TimelineItem(profileImgId3, "김도현", "@KittenDiger", "4일",
-                "구글인ㅇㅎㅇ의 인용의 인용", null, null,
-                1, 2, null, 50),
-        )
-
+        return timelineList.shuffled()
     }
 
     override fun onPause() {
