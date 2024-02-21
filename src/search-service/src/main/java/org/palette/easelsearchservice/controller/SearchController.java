e19e9fdcd9616b14119cdc88bc564f6473fe8358
Wonyoung Lee <74983448+210-reverof@users.noreply.github.com>
2024-02-21 15:02:04 +0900

    feat: add paint response mapping (#268)
    * feat: create createUser interface
    
    * feat: send_email_auth.proto, update_user_auth-status.proto
    
    * refactor : GUpdateUserAuthStatusRequest 필드 수정
    
    * refactor : GSendEmailAuthRequest Protobuf 필드 수정
    
    * refactor : protobuf 파일 네이밍 변경
    
    * feat : Kafka, RabbitMQ 공통 모듈 추가
    
    * refactor : typo
    
    * feat: add UpdateUserImagePath message
    
    * feat: update UpdateUserEvent message
    
    * feat : Internal Error Logging Aspect 추가
    
    * feat: separate quote Paint
    
    * Add 'UNLIKED_PAINT' to TopicConstant, UnlikedPaintEvent
    
    * refactor: Update field in PaintCreatedEvent
    
    * fix: remove ReplyCreatedEvent
    
    * feat: response mapping for gPaintResponse
    
    * feat: remove duplicated response
    
    * feat: add paint entity converter
    
    * feat: add paint response returning
    
    ---------
    
    Co-authored-by: Diger <kitten.diger@gmail.com>
    Co-authored-by: doxxx93 <doxxx93@gmail.com>

--- src/search-service/src/main/java/org/palette/easelsearchservice/controller/SearchController.java
+++ src/search-service/src/main/java/org/palette/easelsearchservice/controller/SearchController.java
@@ -3,13 +3,16 @@ package org.palette.easelsearchservice.controller;
 import lombok.RequiredArgsConstructor;
 import org.palette.easelsearchservice.dto.request.SearchRequest;
 import org.palette.easelsearchservice.dto.response.PaintResponse;
+import org.palette.easelsearchservice.external.groc.GrpcSocialClient;
 import org.palette.easelsearchservice.usecase.SearchUsecase;
+import org.palette.grpc.GGetPaintsByIdsResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,6 +20,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SearchController {
     private final SearchUsecase searchUsecase;
+    private final GrpcSocialClient grpcSocialClient;
 
     @GetMapping("/all")
     public ResponseEntity<List<PaintResponse>> searchAllPaints(@ModelAttribute SearchRequest searchRequest) {
@@ -26,5 +30,13 @@ public class SearchController {
     @GetMapping("/recent")
     public ResponseEntity<PaintResponse> searchRecentPaints(@ModelAttribute SearchRequest searchRequest) {
         return ResponseEntity.ok(searchUsecase.searchRecentPaints());
+    }
+
+    @GetMapping("/test")
+    public void searchRecentPaints() {
+        List<Long> ids = new ArrayList<>();
+        ids.add(1L);
+        ids.add(2L);
+        System.out.println(grpcSocialClient.getPaintsByIds(ids));
     }
 }
--- src/search-service/src/main/java/org/palette/easelsearchservice/dto/response/MediaResponse.java
+++ src/search-service/src/main/java/org/palette/easelsearchservice/dto/response/MediaResponse.java
@@ -1,7 +1,16 @@
 package org.palette.easelsearchservice.dto.response;
 
+import org.palette.grpc.GMediaResponse;
+import org.palette.grpc.GUserResponse;
+
 public record MediaResponse(
         String type,
         String path
 ) {
+    public static MediaResponse from(GMediaResponse gMedia) {
+        return new MediaResponse(
+                gMedia.getType(),
+                gMedia.getPath()
+        );
+    }
 }
