626f2203ef4dc9991d9b17e0381b6125846dc2ed
doxxx93 <doxxx93@gmail.com>
2024-02-22 15:48:43 +0900

    fix: update get topic

--- src/common-module/src/main/java/org/palette/dto/event/PaintViewedEvent.java
+++ src/common-module/src/main/java/org/palette/dto/event/PaintViewedEvent.java
@@ -3,12 +3,9 @@ package org.palette.dto.event;
 import org.palette.dto.EaselEvent;
 
 public record PaintViewedEvent(Long paintId) implements EaselEvent {
-    public static PaintViewedEvent from(Long paintId) {
-        return new PaintViewedEvent(paintId);
-    }
 
     @Override
     public String getTopic() {
-        return TopicConstant.PAINT_VIEWED.value();
+        return TopicConstant.PAINT_VIEWED.value;
     }
 }
